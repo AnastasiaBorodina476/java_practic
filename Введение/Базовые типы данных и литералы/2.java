52b8e002c2a1eeaccdac745c25c20e959b890523
Anastasia <146103430+AnastasiaBorodina476@users.noreply.github.com>
2026-05-28 01:33:08 +0300

    Delete Введение/Базовые типы данных и литералы directory

--- Введение/Базовые типы данных и литералы/2.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // "Настя" в Unicode
-        System.out.println("\u041D\u0430\u0441\u0442\u044F");
-        
-        // "Nastya" в Unicode
-        System.out.println("\u004E\u0061\u0073\u0074\u0079\u0061");
-    }
-}
