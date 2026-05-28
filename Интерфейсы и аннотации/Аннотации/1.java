9e636bf828fe6c2677166929ed51d2686093c143
Anastasia <146103430+AnastasiaBorodina476@users.noreply.github.com>
2026-05-28 12:31:49 +0300

    Update 1.java

--- Интерфейсы и аннотации/Аннотации/1.java
+++ Интерфейсы и аннотации/Аннотации/1.java
@@ -1,1 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.oldMethod();
+        
+        SuppressExample example = new SuppressExample();
+        example.suppressWarningDemo();
+    }
+}
+
+class Parent {
+    public void display() {
+        System.out.println("Parent display");
+    }
+}
+
+class Child extends Parent {
+    @Override
+    public void display() {
+        System.out.println("Child display");
+    }
+    
+    @Deprecated
+    public void oldMethod() {
+        System.out.println("Этот метод устарел, используйте newMethod()");
+    }
+    
+    public void newMethod() {
+        System.out.println("Новый метод");
+    }
+}
+
+class SuppressExample {
+    @SuppressWarnings("unchecked")
+    public void suppressWarningDemo() {
+        List rawList = new ArrayList();
+        rawList.add("Строка");
+        rawList.add(123);
+        
+        for (Object obj : rawList) {
+            System.out.println("Элемент: " + obj);
+        }
+    }
+}
