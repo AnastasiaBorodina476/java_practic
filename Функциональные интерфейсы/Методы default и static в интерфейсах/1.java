ed7c5cf0c20bb050893c388f0022b17c7da87203
Anastasia <146103430+AnastasiaBorodina476@users.noreply.github.com>
2026-05-28 12:36:49 +0300

    Update 1.java

--- Функциональные интерфейсы/Методы default и static в интерфейсах/1.java
+++ Функциональные интерфейсы/Методы default и static в интерфейсах/1.java
@@ -1,1 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        
+        System.out.println("=== Вызов методов ===");
+        
+        obj.defaultMethod();
+        
+        obj.abstractMethod();
+        
+        MyInterface.staticMethod();
+        
+        MyInterface another = new MyClass();
+        another.defaultMethod();
+    }
+}
 
+interface MyInterface {
+    void abstractMethod();
+    
+    default void defaultMethod() {
+        System.out.println("Default метод в интерфейсе");
+    }
+    
+    static void staticMethod() {
+        System.out.println("Static метод в интерфейсе");
+    }
+}
+
+class MyClass implements MyInterface {
+    @Override
+    public void abstractMethod() {
+        System.out.println("Реализация абстрактного метода");
+    }
+    
+    @Override
+    public void defaultMethod() {
+        System.out.println("Переопределенный default метод");
+    }
+}
