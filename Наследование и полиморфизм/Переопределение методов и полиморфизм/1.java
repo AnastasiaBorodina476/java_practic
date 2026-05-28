43f8fb4e8030fe3eef83a4a4887f18c93d12f6ab
Anastasia <146103430+AnastasiaBorodina476@users.noreply.github.com>
2026-05-28 12:19:26 +0300

    Update 1.java

--- Наследование и полиморфизм/Переопределение методов и полиморфизм/1.java
+++ Наследование и полиморфизм/Переопределение методов и полиморфизм/1.java
@@ -1,1 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        
+        System.out.println(calc.add(5, 10));
+        System.out.println(calc.add(5, 10, 15));
+        System.out.println(calc.add(5.5, 10.5));
+        System.out.println(calc.add("5", "10"));
+    }
+}
 
+class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    
+    public double add(double a, double b) {
+        return a + b;
+    }
+    
+    public int add(String a, String b) {
+        return Integer.parseInt(a) + Integer.parseInt(b);
+    }
+}
