bfeb7bb526dad6ecce3d5096e23fd676f6757f1f
Anastasia <146103430+AnastasiaBorodina476@users.noreply.github.com>
2026-05-28 12:17:32 +0300

    Update 1.java

--- Наследование и полиморфизм/Использование super и this/1.java
+++ Наследование и полиморфизм/Использование super и this/1.java
@@ -1,1 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Бобик", 3, "Овчарка");
+        dog.display();
+    }
+}
 
+class Animal {
+    String name;
+    int age;
+    
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("Конструктор Animal");
+    }
+    
+    void makeSound() {
+        System.out.println("Животное издает звук");
+    }
+}
+
+class Dog extends Animal {
+    String breed;
+    
+    Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+        System.out.println("Конструктор Dog");
+    }
+    
+    void display() {
+        System.out.println("\n=== Демонстрация super ===");
+        
+        System.out.println("1. Доступ к полю суперкласса: name = " + super.name);
+        System.out.println("   Доступ к полю суперкласса: age = " + super.age);
+        
+        System.out.print("2. Доступ к методу суперкласса: ");
+        super.makeSound();
+        
+        System.out.println("3. Вызов конструктора суперкласса уже выполнен (super(name, age))");
+    }
+}
