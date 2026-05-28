e699a5b4fc23b623fe15f2200ad817904f189c8f
Anastasia <146103430+AnastasiaBorodina476@users.noreply.github.com>
2026-05-28 12:04:00 +0300

    Update 1.java

--- Классы и методы/Модификатор final и неизменяемость/1.java
+++ Классы и методы/Модификатор final и неизменяемость/1.java
@@ -1,1 +1,21 @@
+class A {
+    public final int a;
+    
+    public A() {
+        a = 10;
+    }
+    
+    public A(int value) {
+        a = value;
+    }
+}
 
+public class Main {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        System.out.println(obj1.a);
+        
+        A obj2 = new A(30);
+        System.out.println(obj2.a);
+    }
+}
