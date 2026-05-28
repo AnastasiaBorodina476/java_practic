166569635f7f4f83357cc1ae60b83b0df7df335e
Anastasia <146103430+AnastasiaBorodina476@users.noreply.github.com>
2026-05-28 12:15:54 +0300

    Update 1.java

--- Наследование и полиморфизм/Классы и методы final/1.java
+++ Наследование и полиморфизм/Классы и методы final/1.java
@@ -1,1 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.show();
+    }
+}
 
+class Parent {
+    public final void show() {
+        System.out.println("Родительский метод final - нельзя переопределить");
+    }
+}
+
+class Child extends Parent {
+    // Ошибка компиляции!
+    // public void show() {
+    //     System.out.println("Попытка переопределить final-метод");
+    // }
+}
